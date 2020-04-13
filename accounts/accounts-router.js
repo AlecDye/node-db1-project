dd7b8a03433f044ecc8a66ee67f689318af861b2
Alec Dye <alecdye@gmail.com>
2020-04-13 15:41:13 -0700

    Testing CRUD endpoints with Postman

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -5,6 +5,7 @@ const db = require("../data/dbConfig");
 const router = express.Router();
 
 // CRUD operations
+// success
 router.get("/", (req, res) => {
   db.select("*")
     .from("accounts")
@@ -17,12 +18,79 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {});
+// success
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db("accounts")
+    .where({ id: id })
+    .first()
+    .then((account) => {
+      res.status(200).json({ data: account });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
 
-router.post("/", (req, res) => {});
+// success
+router.post("/", (req, res) => {
+  const accountData = req.body;
+  db("accounts")
+    .insert(accountData, "id")
+    .then((account) => {
+      const id = ids[0];
+      db("accounts")
+        .where({ id })
+        .first()
+        .then((account) => {
+          res.status(200).json({ data: account });
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
 
-router.put("/:id", (req, res) => {});
+// success
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  db("accounts")
+    .where({ id })
+    .update(changes)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "Account update successful" });
+      } else {
+        res.status(404).json({ message: "Account was not found" });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db("accounts")
+    .where({ id })
+    .del()
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "Account deletion successful" });
+      } else {
+        res.status(404).json({
+          message: "Account was not found (it may have been deleted)",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
 
 module.exports = router;
