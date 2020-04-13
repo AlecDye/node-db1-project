2bf4527b59d714a2b44c7d186572b84bccd16d5d
Alec Dye <alecdye@gmail.com>
2020-04-13 14:52:09 -0700

    Building API CRUD endpoints

---
+++ accounts/accounts-router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+// CRUD operations
+router.get("/", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .then((accounts) => {
+      res.status(200).json({ data: accounts });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: error.message });
+    });
+});
+
+router.get("/:id", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/:id", (req, res) => {});
+
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
